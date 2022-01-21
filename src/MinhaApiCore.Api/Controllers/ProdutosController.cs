0e91e3f9da6d4dbbbd71bb6b12a9a45289e8015c
Diego Luques <luquesdiego@outlook.com>
2022-01-21 00:27:34 -0400

    Implementação de ProdutosController

---
+++ src/MinhaApiCore.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MinhaApiCore.Api.ViewModels;
+using MinhaApiCore.Business.Intefaces;
+using MinhaApiCore.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaApiCore.Api.Controllers
+{
+    [Route("api/produtos")]
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(INotificador notificador,
+                                  IProdutoRepository produtoRepository,
+                                  IProdutoService produtoService,
+                                  IMapper mapper) : base(notificador)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid Id)
+        {
+            var produtoViewModel = await ObterProduto(Id);
+
+            if (produtoViewModel == null) return NotFound();
+
+            return CustomResponse(produtoViewModel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var imagemNome = Guid.NewGuid() + "_" + produtoViewModel.Imagem;
+            if (!UploadArquivo(produtoViewModel.ImagemUpload, imagemNome))
+            {
+                return CustomResponse();
+            }
+
+            produtoViewModel.Imagem = imagemNome;
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return CustomResponse(produtoViewModel);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Excluir(Guid Id)
+        {
+            var produtoVw = await ObterProduto(Id);
+
+            if (produtoVw == null) return NotFound();
+
+            await _produtoService.Remover(Id);
+
+            return CustomResponse(produtoVw);
+        }
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid Id)
+        {
+            return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutoFornecedor(Id));
+        }
+
+        private bool UploadArquivo(string arquivo, string imgNome)
+        {
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
+
+            if (string.IsNullOrEmpty(arquivo))
+            {
+                NotificarErro("Forneça uma imagem para este produto!");
+                return false;
+            }
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                NotificarErro("Já existe um arquivo com este nome!");
+                return false;
+            }
+
+            System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
+
+            return true;
+        }
+    }
+}
